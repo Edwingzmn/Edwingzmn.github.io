78a6433e2b5e867d041bb62f06846bc5213f8f80
Edwingzmn <eguzmann1601@alumno.ipn.mx>
2021-02-12 15:44:47 -0600

    Edwingzmn
    Commit inicial

---
+++ js/javaS3.js
@@ -0,0 +1,36 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '525579125346';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let nombre_p = document.querySelector('#nombre_p').value
+        let hospital = document.querySelector('#hospital').value
+        let diaconsulta = document.querySelector('#diaconsulta').value
+        let mensaje = 'send?phone=' + telefono + '&text=Formulario enviado desde la página web...%0ANombre del padre o madre:%0A*' + nombre + '*%0ANombre del paciente:%0A*' + nombre_p + '*%0AHospital: %0A*' + hospital + '*%0ADía de consulta: %0A*' + diaconsulta + '*'
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
